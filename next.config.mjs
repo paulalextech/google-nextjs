de0eebad6c4c14999350636baf414af914797dd6
paulalextech <psteane123@hotmail.com>
2024-03-05 15:49:35 +0000

    add body section

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'blog.hubspot.com',
+				pathname: '**',
+			},
+		],
+	},
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
 import HomeHeader from '@/components/HomeHeader';
 import React from 'react';
+import Image from 'next/image';
+import HomeSearch from '@/components/HomeSearch';
 
 export default function Home() {
 	return (
 		<>
 			<HomeHeader />
+			<div className="flex flex-col items-center mt-24">
+				<Image
+					src="https://blog.hubspot.com/hs-fs/hubfs/The%20Secret%20History%20of%20the%20Google%20Logo-7.jpeg?width=297&name=The%20Secret%20History%20of%20the%20Google%20Logo-7.jpeg"
+					alt="Google logo"
+					width={300}
+					height={100}
+					priority
+					style={{ width: 'auto' }}
+				/>
+
+				<HomeSearch />
+			</div>
 		</>
 	);
 }
