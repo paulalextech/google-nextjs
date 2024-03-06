6cd57e88f88fd516738705998bae577d1e398ac2
paulalextech <psteane123@hotmail.com>
2024-03-06 17:06:31 +0000

    add image results

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import ImageSearchResults from '@/components/ImageSearchResults';
+import Link from 'next/link';
 
-export default function ImageSearchPage() {
-	return <div className="text-red-500">ImageSearchPage</div>;
+export default async function ImageSearchPage({ searchParams }) {
+	const response = await fetch(
+		`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image`
+	);
+	if (!response.ok) throw new Error('Something went belly up!');
+	const data = await response.json();
+	const results = data.items;
+
+	if (!results) {
+		return (
+			<div className="flex flex-col justify-center items-center pt-10">
+				<h1 className="text-3xl mb-4">
+					No results found amigo! {searchParams.searchTerm}
+				</h1>
+				<p className="text-lg">
+					Try searching the web or images for something else{' '}
+					<Link href="/" className="text-blue-500">
+						Home
+					</Link>
+				</p>
+			</div>
+		);
+	}
+
+	return (
+		// eslint-disable-next-line react/jsx-key
+		<div>{results && <ImageSearchResults results={data} />}</div>
+	);
 }
