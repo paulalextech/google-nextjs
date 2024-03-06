7fa8e34d6406dca3cc2321aabc9c16da7e411385
paulalextech <psteane123@hotmail.com>
2024-03-06 14:25:06 +0000

    complete search header options

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import SearchBox from './SearchBox';
 import { RiSettings3Line } from 'react-icons/ri';
 import { TbGridDots } from 'react-icons/tb';
+import SearchHeaderOptions from './SearchHeaderOptions';
 
 export default function SearchHeader() {
 	return (
@@ -31,6 +32,8 @@ export default function SearchHeader() {
 					Sign in
 				</button>
 			</div>
+
+			<SearchHeaderOptions />
 		</header>
 	);
 }
