7c14836aa7a30fd9fea060df4ba9d55897f97247
paulalextech <psteane123@hotmail.com>
2024-03-06 16:03:17 +0000

    handle errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,32 @@
-export default function WebSearchPage() {
-	return <div>WebSearchPage</div>;
+// import WebSearchResults from '@/components/WebSearchResults';
+import Link from 'next/link';
+
+export default async function WebSearchPage({ searchParams }) {
+	const response = await fetch(
+		`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}x&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+	);
+	if (!response.ok) throw new Error('Something went belly up!');
+	const data = await response.json();
+	const results = data.items;
+
+	if (!results) {
+		return (
+			<div className="flex flex-col justify-center items-center pt-10">
+				<h1 className="text-3xl mb-4">
+					No results found amigo! {searchParams.searchTerm}
+				</h1>
+				<p className="text-lg">
+					Try searching the web or images for something else{' '}
+					<Link href="/" className="text-blue-500">
+						Home
+					</Link>
+				</p>
+			</div>
+		);
+	}
+
+	return (
+		// eslint-disable-next-line react/jsx-key
+		<div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+	);
 }
