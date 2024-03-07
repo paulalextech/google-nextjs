670efd6bd80514be1a85ea10bcf70529e8c1633a
paulalextech <psteane123@hotmail.com>
2024-03-07 09:20:25 +0000

    add loading effect

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,11 @@
 // import WebSearchResults from '@/components/WebSearchResults';
 import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link';
+import { resolve } from 'styled-jsx/css';
 
 export default async function WebSearchPage({ searchParams }) {
 	const startIndex = searchParams.start || '1';
+	await new Promise((resolve) => setTimeout(resolve, 1000));
 	const response = await fetch(
 		`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
 	);
